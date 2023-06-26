b8ddce16fd707d178280dd6dfa3f266af1839469
Andrii Prykhodko <andriichello@gmail.com>
2023-06-26 22:36:50 +0300

    Add padding to categories navbar

--- src/components/preview/category/PreviewCategoryNavBar.vue
+++ src/components/preview/category/PreviewCategoryNavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex flex-col justify-center bg-base-100 shadow-md pb-1 mb-2" v-if="categories && categories.length">
     <div class="w-full flex justify-center items-center">
-      <div class="max-w-full flex justify-start px-2 pt-2 gap-2 overflow-x-auto overflow-y-hidden" id="category-buttons-scroll">
+      <div class="max-w-full flex justify-start gap-2 p-2 overflow-x-auto overflow-y-hidden" id="category-buttons-scroll">
         <template v-for="c in categories" :key="c.id">
           <Category :item="c" :id="`category-${c.id}-button`"
                     :selected="category && category.id === c.id"
