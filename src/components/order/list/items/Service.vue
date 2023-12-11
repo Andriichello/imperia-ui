26bf425b280a8d96a093d2349a1d5b7aa4bf2aca
Andrii Prykhodko <andriichello@gmail.com>
2023-12-11 15:38:03 +0200

    Create Service order component

--- src/components/order/list/items/Service.vue
+++ src/components/order/list/items/Service.vue
@@ -1,23 +1,11 @@
 <template>
   <div class="card w-full bg-base-100 shadow-xl">
-    <div class="w-full max-h-[180px] select-none" v-if="image">
-      <figure class="w-full max-h-[180px] rounded-t-box" >
-        <img :alt="title" :src="image"
-             class="overflow-hidden"
-             style="object-fit: cover;"
-             @load="imageLoaded = true"/>
-      </figure>
-    </div>
-
     <div class="card-body min-h-[100px]">
-      <div class="flex justify-between items-center card-title">
+      <div class="flex justify-between items-start card-title">
         <div class="grow flex justify-center items-start card-title">
           <h2 class="grow line-clamp-2 text-ellipsis flex justify-start items-center">
             {{ title }}
           </h2>
-          <div class="badge badge-warning mt-1 select-none" v-if="badge && badge.length">
-            {{ badge }}
-          </div>
         </div>
 
         <div class="flex flex-nowrap justify-end items-end gap-2">
@@ -42,13 +30,11 @@
             </BaseIcon>
           </div>
 
-          <button class="btn btn-sm btn-square btn-outline" :class="{'btn-active': this.field}"
-                  @click="onToggle">
-            <BaseIcon width="32" height="32" color="currentColor">
-              <path d="M16.8395 9.7857C17.1911 9.4341 17.1911 8.8653 16.8395 8.5137C16.4879 8.1621 15.9191 8.1621 15.5675 8.5137L10.5083 13.5717L8.29547 11.3613C7.94387 11.0097 7.37507 11.0097 7.02347 11.3613C6.67187 11.7129 6.67187 12.2817 7.02347 12.6333L9.87226 15.4809C10.0475 15.6573 10.2791 15.7449 10.5083 15.7449C10.7387 15.7449 10.9691 15.6573 11.1443 15.4809L16.8395 9.7857Z" fill="white"/>
-            </BaseIcon>
-          </button>
+          <Counter class="self-start ml-1" v-if="rerendered"
+                   @on-change="onChangeAmount"
+                   :amount="amount"/>
         </div>
+
       </div>
 
       <div class="flex-grow">
@@ -57,36 +43,26 @@
         </p>
       </div>
 
-      <div class="card-actions justify-between items-end pt-2">
-        <div class="flex flex-wrap flex-grow flex-row justify-start items-center gap-4">
-          <div class="flex justify-center items-center gap-2">
-            <span class="font-normal text-sm">{{$t('preview.space.floor')}}: </span>
-            <span class="font-bold text-lg min-w-[16px]">{{ floor }}</span>
+      <div class="card-actions justify-end items-start">
+        <label class="form-control w-full max-w-[100px] max-h-[50px]">
+          <input class="input input-bordered input-sm text-lg w-full max-w-xs"
+                 type="number" min="0" v-model="duration"/>
+
+          <div class="label translate-y-[-6px]">
+            <span class="label-text-alt"></span>
+            <span class="label-text-alt">Тривалість (хв)</span>
           </div>
+        </label>
+      </div>
 
-          <div class="flex justify-center items-center gap-2">
-            <span class="font-normal text-sm">{{$t('preview.space.number')}}: </span>
-            <span class="font-bold text-lg min-w-[16px]">{{ number }}</span>
-          </div>
-        </div>
+      <div class="card-actions justify-between items-start pt-2">
+        <h2 class="card-title">
+          {{ oncePaidPrice }}
+        </h2>
 
-        <div class="breadcrumbs p-0 justify-end">
-          <ul>
-            <li>
-              <h2 class="card-title grow">
-                {{ price }}
-              </h2>
-            </li>
-
-            <template v-for="alternation in alternations" :key="alternation.id">
-              <li v-if="alternation.metadata?.price">
-                <h2 class="card-title text-sm grow self-end">
-                  {{ priceFormatted(alternation.metadata?.price) }}
-                </h2>
-              </li>
-            </template>
-          </ul>
-        </div>
+        <h2 class="card-title">
+          {{ hourlyPaidPrice }} / год.
+        </h2>
       </div>
 
       <CommentList :comments="comments" class="w-full" v-if="comments?.length"
@@ -98,44 +74,49 @@
 <script>
 import { defineComponent } from "vue";
 import {mapActions, mapGetters} from "vuex";
+import Counter from "@/components/preview/list/items/Counter.vue";
 import {priceFormatted} from "@/helpers";
-import BaseIcon from "@/components/icons/BaseIcon.vue";
 import CommentList from "@/components/order/comment/CommentList.vue";
+import BaseIcon from "@/components/icons/BaseIcon.vue";
 
 export default defineComponent({
   // eslint-disable-next-line
-  name: "Space",
-  components: {CommentList, BaseIcon},
+  name: "Service",
+  components: {BaseIcon, CommentList, Counter},
   props: {
-    spaceId: Number,
+    serviceId: Number,
   },
   data() {
     return {
-      imageLoaded: false,
+      rerendered: true,
     };
   },
   computed: {
     ...mapGetters({
       theme: "theme/get",
+    }),
+    ...mapGetters({
+      theme: "theme/get",
       authorized: "auth/authorized",
-      fields: "order/spaces",
+      fields: "order/services",
     }),
     isPreview() {
       return (this.$route.name ?? '').startsWith('preview');
     },
+    item() {
+      return this.$store.getters['order/orderedService'](this.serviceId)
+          ?? this.$store.getters['preview/service'](this.serviceId);
+    },
     field() {
-      const fields = this.fields.filter(
-          (s) => s.spaceId === this.id
+      return this.serviceFields[0];
+    },
+    serviceFields() {
+      return this.$store.getters['order/services'].filter(
+          (p) => p.serviceId === this.id
       );
-
-      return fields[0] ?? null;
     },
     id() {
-      return this.spaceId;
-    },
-    item() {
-      return this.$store.getters['order/orderedSpace'](this.spaceId)
-          ?? this.$store.getters['preview/space'](this.spaceId);
+      return this.item.id;
     },
     title() {
       return this.item.title;
@@ -146,12 +127,6 @@ export default defineComponent({
     description() {
       return this.item.description;
     },
-    floor() {
-      return this.item.floor;
-    },
-    number() {
-      return this.item.number;
-    },
     image() {
       const s = this.item;
 
@@ -161,8 +136,22 @@ export default defineComponent({
 
       return null;
     },
-    price() {
-      return priceFormatted(this.item?.price);
+    amount() {
+      return this.field?.amount;
+    },
+    oncePaidPrice() {
+      return priceFormatted(this.item?.oncePaidPrice);
+    },
+    hourlyPaidPrice() {
+      return priceFormatted(this.item?.hourlyPaidPrice);
+    },
+    duration: {
+      get() {
+        return this.field?.duration;
+      },
+      set(value) {
+        this.onChangeDuration(value);
+      },
     },
     categories() {
       const ids = this.item.categoryIds ?? [];
@@ -171,26 +160,53 @@ export default defineComponent({
         return [];
       }
 
-      return this.$store.getters['preview/spaceCategories']
+      return this.$store.getters['preview/serviceCategories']
           .filter(category => ids.includes(category.id));
     },
-    alternations() {
-      return this.item?.pendingAlterations ?? [];
-    },
     comments() {
-      if (!this.field || !this.field?.comments) {
-        return [];
-      }
-
       return this.field?.comments ?? [];
     },
   },
   methods: {
     priceFormatted,
     ...mapActions({
-      setField: 'order/setSpace',
-      unsetField: 'order/unsetSpace',
+      setField: 'order/setService',
+      unsetField: 'order/unsetService',
     }),
+    onChangeAmount({amount}) {
+      if (amount === null) {
+        this.unsetField({
+          serviceId: this.id,
+        });
+
+        return;
+      }
+
+      this.setField({
+        serviceId: this.id,
+        amount: amount,
+        duration: this.duration,
+        comments: this.comments,
+      });
+    },
+    onChangeDuration(duration) {
+      this.setField({
+        serviceId: this.id,
+        amount: this.amount,
+        duration: duration,
+        comments: this.field?.comments ?? [],
+      });
+    },
+    async freshRender() {
+      // Remove MyComponent from the DOM
+      this.rerendered = false;
+
+      // Wait for the change to get flushed to the DOM
+      await this.$nextTick();
+
+      // Add the component back in
+      this.rerendered = true;
+    },
     onAddComment() {
       if (!this.field) {
         return;
@@ -226,21 +242,11 @@ export default defineComponent({
       }
 
       this.setField({
-        spaceId: this.spaceId,
+        serviceId: this.id,
+        amount: this.field?.amount,
+        duration: this.field?.duration,
         comments: comments
       });
-    },
-    onToggle() {
-      if (this.field) {
-        this.unsetField({
-          spaceId: this.id
-        });
-      } else {
-        this.setField({
-          spaceId: this.id,
-          comments: this.field?.comments ?? [],
-        });
-      }
     },
   },
 });
