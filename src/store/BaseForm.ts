20efaeaaf803616cb672beba51a3b47349a87387
Andrii Prykhodko <andriichello@gmail.com>
2024-11-03 12:45:06 +0200

    Create BaseForm

---
+++ src/store/BaseForm.ts
@@ -0,0 +1,194 @@
+export default class BaseForm <T extends object = object> {
+  /** Resource that was used to populate the form. */
+  protected resource: T | null;
+
+  /** Properties that were set to the form. */
+  protected properties: T;
+
+  /** Changes that were made to the form's properties. */
+  protected changes: Partial<T>;
+
+  /**
+   * BaseForm's constructor.
+   *
+   * @param resource
+   */
+  constructor(resource: T = null) {
+    this.resource = resource;
+    this.properties = {} as T;
+    this.changes = {} as Partial<T>;
+
+    this.populate(resource);
+  }
+
+  /**
+   * Dynamically populate properties from the given data object.
+   *
+   * @param data
+   */
+  public populate(data: Partial<T> = {}) {
+    for (const [key, value] of Object.entries(data)) {
+      this.properties[key] = value;
+    }
+  }
+
+  /**
+   * Dynamically repopulate properties from the given data object.
+   * Sets `properties` to an empty object before setting new values.
+   *
+   * @param data
+   *
+   * @return void
+   */
+  public repopulate(data: Partial<T> = {}): void {
+    this.properties = {} as T;
+    this.populate(data);
+  }
+
+  /**
+   * Returns all the form's properties.
+   *
+   * @return object
+   */
+  public getProperties(): Partial<T> {
+    return this.properties;
+  }
+
+  /**
+   * Sets property on the form.
+   *
+   * @return object
+   */
+  public setProperty(name: string, value: unknown, silent = false): void {
+    if (silent === false && this.differs(name, value)) {
+      this.setChange(name, value);
+    }
+
+    this.properties[name] = value;
+  }
+
+  /**
+   * Removes property from the form.
+   *
+   * @param name
+   *
+   * @return boolean
+   */
+  public unsetProperty(name: string): boolean {
+    this.unsetChange(name);
+    return delete this.properties[name];
+  }
+
+  /**
+   * Removes all the properties.
+   *
+   * @return void
+   */
+  public clearProperties(): void {
+    this.properties = {} as T;
+  }
+
+  /**
+   * Determines if property's value differs from the given value.
+   *
+   * @param name Name of the property
+   * @param value Value to be compared against
+   *
+   * @return boolean
+   */
+  public differs(name: string, value: unknown): boolean {
+    return this.properties[name] !== value;
+  }
+
+  /**
+   * Returns all the form's properties that were changed.
+   *
+   * @return object
+   */
+  public getChanges(): Partial<T> {
+    return this.changes;
+  }
+
+  /**
+   * Returns all property's changed value.
+   *
+   * @param name
+   * @param defaultValue
+   *
+   * @return any
+   */
+  public getChange(name: string, defaultValue = undefined): unknown {
+    if (Object.prototype.hasOwnProperty.call(this.changes, name)) {
+      return this.changes[name];
+    }
+
+    return defaultValue;
+  }
+
+  /**
+   * Records that the property was changed (and it's new value).
+   *
+   * @param name
+   * @param value
+   *
+   * @return void
+   */
+  public setChange(name: string, value: unknown): void {
+    this.changes[name] = value;
+  }
+
+  /**
+   * Removes the record about property being changed.
+   *
+   * @param name
+   *
+   * @return boolean
+   */
+  public unsetChange(name: string): boolean {
+    return delete this.changes[name];
+  }
+
+  /**
+   * Removes all the records about properties being changed.
+   *
+   * @return void
+   */
+  public clearChanges(): void {
+    this.changes = {};
+  }
+
+  /**
+   * Determines if there is at least one record about
+   * properties being changed.
+   *
+   * @return boolean
+   */
+  public hasChanges(): boolean {
+    return Object.keys(this.getChanges()).length > 0;
+  }
+
+  /**
+   * Determines if there is at least one record about
+   * properties being changed and at the same time the
+   * resource property's value differs from the new one.
+   *
+   * @return boolean
+   */
+  public hasRealChanges(): boolean {
+    if (!this.resource) {
+      return this.hasChanges();
+    }
+
+    let result = false;
+
+    Object.keys(this.getChanges())
+      .forEach(name => {
+        if (this.resource[name] !== this.getChange(name)) {
+          result = true;
+          return;
+        }
+      });
+
+    return result;
+  }
+}
