cf3365f84f29b00b1346a4af9782c4bb074f1d9e
Andrii Prykhodko <andriichello@gmail.com>
2024-11-03 15:35:20 +0200

    Create CrudState

---
+++ src/store/CrudState.ts
@@ -0,0 +1,75 @@
+import BaseForm from "@/store/BaseForm";
+
+export default class CrudState <
+  T extends object = object,
+  F extends BaseForm<T> = BaseForm<T>,
+  I = Response,
+  S = Response,
+  C = Response,
+  U = Response,
+  D = Response,
+> {
+  /** Resource form that is currently used. */
+  public form: F | null;
+
+  /** Resource that is currently selected. */
+  public selected: T | null;
+
+  /** Resource that was retrieved in a last show request. */
+  public resource: T | null;
+
+  /** Resources that was retrieved in a last index request. */
+  public resources: T[] | null;
+
+  /** Last index response. */
+  public index: I | Response | null;
+
+  /** Last show response. */
+  public show: S | Response | null;
+
+  /** Last store response. */
+  public store: C | Response | null;
+
+  /** Last update response. */
+  public update: U | Response | null;
+
+  /** Last delete response. */
+  public destroy: D | Response | null;
+
+  /** Is `true` if currently waiting for index response.  */
+  public isLoadingIndex: boolean;
+
+  /** Is `true` if currently waiting for show response.  */
+  public isLoadingShow: boolean;
+
+  /** Is `true` if currently waiting for store response.  */
+  public isLoadingStore: boolean;
+
+  /** Is `true` if currently waiting for update response.  */
+  public isLoadingUpdate: boolean;
+
+  /** Is `true` if currently waiting for destroy response.  */
+  public isLoadingDestroy: boolean;
+
+  /**
+   * CrudState's constructor.
+   *
+   * @param FormClass
+   */
+  constructor(FormClass: new () => F) {
+    this.selected = null;
+    this.form = new FormClass();
+
+    this.index = null;
+    this.show = null;
+    this.store = null;
+    this.update = null;
+    this.destroy = null;
+
+    this.isLoadingIndex = false;
+    this.isLoadingShow = false;
+    this.isLoadingStore = false;
+    this.isLoadingUpdate = false;
+    this.isLoadingDestroy = false;
+  }
+}
